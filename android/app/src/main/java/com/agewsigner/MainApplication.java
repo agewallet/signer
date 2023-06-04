70264dfa19049d967c9ea5717ce1fe1eb84b918f
Alexandr Voroncov <defauthack@gmail.com>
2023-06-04 21:11:32 +0400

    refactor: rename project

--- android/app/src/main/java/com/agewsigner/MainApplication.java
+++ android/app/src/main/java/com/agewsigner/MainApplication.java
@@ -1,4 +1,4 @@
-package com.signer;
+package com.agewsigner;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
--- ios/AGEWSignerTests/AGEWSignerTests.m
+++ ios/AGEWSignerTests/AGEWSignerTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface signerTests : XCTestCase
+@interface AGEWSignerTests : XCTestCase
 
 @end
 
-@implementation signerTests
+@implementation AGEWSignerTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
